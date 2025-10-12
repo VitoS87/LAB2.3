296b4b12ac86f56dbb7a51e6816e77c963509479
Vitos <rustamminis@gmail.com>
2025-10-12 17:41:47 +0200

    Add files via upload

---
+++ LAB2.3/LAB2.3(2)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+double b = 4.5;
+double m = -2;
+double n = 2.2;
+double z = Math.Abs(b * m);
+double y;
+if (z > Math.Pow(x, 2)) { y = Math.Sin(b * m + Math.Cos(n * x)); Console.WriteLine("y= " + y); }
+if (z < Math.Pow(x, 2)) { y = Math.Cos(b * m - Math.Sin(x)); Console.WriteLine("y= " + y); }
+if (z == Math.Pow(x, 2)) { y = Math.Sqrt(Math.Pow(Math.Log(10), Math.Abs(Math.Cos(x)))) + Math.Sqrt(Math.Abs(b * m * x)); Console.WriteLine("y= " + y); }
